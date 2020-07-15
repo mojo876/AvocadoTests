0d1c4a4ca5579271e2530802b6b8350770d8dcd8
mojo876 <67339335+mojo876@users.noreply.github.com>
2020-07-15 13:46:50 -0400

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,1 @@
+/usr/local/bin/avocado run /root/avocado-fvt-wrapper/avocado-misc-tests-master/io/net/net_tools.py -m \/root/avocado-fvt-wrapper/avocado-misc-tests-master/io/net/net_tools.py.data/net_tools.yaml
